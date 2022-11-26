79fa4c898d29f2f13135be86463fc75432ea4fd3
Lux14 <magnito14@pm.me>
2022-11-26 14:26:04 -0500

    Made some changes.

--- bot.py
+++ bot.py
@@ -1,32 +1,32 @@
-import discord # Main Discord Package
-import os # Used for executing console commands
-import platform # Used for detecting the OS
+import discord # Main Discord Package.
+from os import system # Used for executing console commands.
+from platform import system as syst # Used for detecting the OS.
 
 from discord.ext import commands # Do not rename or remove!
 
-# Configuration
-prefix = "!" # The prefix for your bot
-token = "YourTokenhere" # The token of your bot
+''' ! Bot Configuration ! '''
+bot_prefix = '>' # The prefix for your bot.
+bot_token = 'Token Goes Here' # The token of your bot.
 
-bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
+bot = commands.Bot(command_prefix=bot_prefix, intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    if 'Windows' in platform.system():
-        os.system("cls")
-        os.system("title 'Discord Python Example'")
-    elif 'Linux' in platform.system():
-        os.system("clear")
-    print(f"> Logged in as {bot.user}")
-    print(f"> Current ping: {round(bot.latency * 1000)} ms")
+    if 'Windows' in syst():
+        system('cls')
+    elif 'Linux' in syst():
+        system('clear')
+
+    print(f'> Logged in as {bot.user}!')
+    print(f'> Current ping: {round(bot.latency * 1000)} MS.')
 
 @bot.command()
 async def ping(ctx):
-    await ctx.reply(f"Responded within {round(bot.latency * 1000)} ms")
+    await ctx.reply(f'Responded within {round(bot.latency * 1000)} MS.')
 
 @bot.command()
 async def say(ctx, *arg):
-    words = ' '.join(arg)
+    words = ' '.join(arg) 
     await ctx.reply(words)
 
-bot.run(token)
+bot.run(bot_token)
